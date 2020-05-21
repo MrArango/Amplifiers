93be4833d75ce1141be6e1de5d5a2c095725bb3e
Arango <bolanosjuan84@gmail.com>
2020-05-20 19:23:36 -0500

    Se eliminan las clases  y contorladores que incluia la plantilla (WeatherForecast)
    Se implementa el Controlador Amplifier, con la funcionalidad de peticion de TODOS los aplificadores (Get) y con manejo de errores
    
    Se crea la clase Respon con la finalidad de ser el objeto de respuesta a las peticiones de la API

---
+++ AmplifiersAPI/Controllers/AmplifierController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amplifiers;
+using AmplifiersAPI.Controllers.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace AmplifiersAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AmplifierController : ControllerBase
+    {
+        private readonly ILogger<AmplifierController> _logger;
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Respon res = new Respon();
+            try
+            {
+                using (var db = new SqlConnection(Properties.Resources.strcon))
+                {
+                    IEnumerable<Amplifier> amplifiers = db.Query<Amplifier>("select Id_Amplifier, Name, BassKnob, MidKnob, TrebleKnob from Amplifiers");
+
+                    res.Valid = true;
+                    res.Data = amplifiers;
+                    res.Message = "Petición de Amplificadores OK";
+
+                    _logger.LogInformation("Petición de todos los Amplificadores OK");
+
+                    return Ok(res);
+                }
+            }
+            catch (Exception ex)
+            {
+                res.Valid = false;
+                res.Data = null;
+
+                res.Message = "La petición de Amplificadores Fallo";
+                _logger.LogInformation(ex.Message);
+                return Ok();
+            }
+
+        }
+
+        public AmplifierController(ILogger<AmplifierController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
---
+++ AmplifiersAPI/Controllers/Responses/Respon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmplifiersAPI.Controllers.Responses
+{
+    public class Respon
+    {
+        public bool Valid { get; set; }
+
+        public string Message { get; set; }
+
+        public object Data { get; set; }
+
+
+        public Respon()
+        {
+            Valid = true;
+        }
+    }
+}
